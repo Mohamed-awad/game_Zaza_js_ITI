5bed813e7504450a04754d924329644e8db9b510
Motaz <motazibrahim@outlook.com>
2018-12-31 20:29:07 +0200

    Code enhancement and new features Reaarranging classes I've rearranged classes and function and added the following - Collosion between fireballs and rockes - Adding new variables for scores - Detect rocks when it reaches the bottom of the screen - Reapare for adding levels - ------- The collision of game over and handling lives is needed to be implemented

--- js/js.js
+++ js/js.js
@@ -1,117 +1,44 @@
 // Start the game with different id;
-
 /// the global variables and arrays
 let container = document.getElementById("container");
 //The array of falling rocks and the array of fireballs
 let fireballs = [];
-let sq = document.getElementsByClassName("obj");
-let currentBall = null;
-let currentRock = null;
 let fireballsID = 10000;
-//Get the width of the window to make the space ship position at the middle exactly
-const windowWidth = window.innerWidth;
-//using window height to make rocks fall
-let windoHeight = window.innerHeight;
-
-
-//this from samman file
-
-
+let currentBall = null;
 let rocksArray = [];
 let rockId = 0;
-let fallingPosition = null;
+let currentRock = null;
+let playerSpaceShip = null; // constructing it to be used in fire ball class
+//Creating arrays off high score // array of survival time // array of rocks destroyed
+let scores = {
+    highScore:[],
+    highestSurvivalTime :[],
+    highestRockDestroyed:[]
+};
+let currentLevel = null;
+let currentScore = null;
+let currentSurvivalTime = null;
 
+
+//Get the width of the window to make the space ship position at the middle exactly
+const windowWidth = container.offsetWidth;
 //class rocks
-
 class Rocks {
     constructor(src) {
+        let fallingPosition = Math.floor(Math.random() * windowWidth);
         this.rock = document.createElement("img");
         this.rock.src = src;
         this.rock.id = `${rockId++}`;
-        // this.rock.className = "obj";
         this.rock.style.position = "absolute";
         this.rock.style.top = "-100px";
         this.rock.style.left = `${fallingPosition}px`;
         this.rock.style.width="150px";
         this.rock.style.height="150px";
+        this.lives = null;
         container.appendChild(this.rock);
+        rocksArray.push(this.rock);
     }
 }
-
-//function levels and fallen
-
-let counter = 0;
-let determineLevel = function(ch)
-{
-    fallingPosition = Math.floor(Math.random()*windowWidth)
-
-    // if(fallingPosition >= window.innerWidth){
-    //     fallingPosition -= 100;
-    // }else if(fallingPosition <= 100){
-    //     fallingPosition += 100;
-    // }else{
-    //     fallingPosition = fallingPosition;
-    // }
-
-    fallingPosition = Math.floor(Math.random()*windowWidth);
-    let movedown = function () {
-        fallenRock.rock.style.top = `${fallenRock.rock.offsetTop + 10}px`
-    };
-    let fallenRock;
-    let chh = ch;
-    console.log("ch", ch);
-    switch (chh) {
-        case 1:
-            fallenRock = new Rocks("./img/rock.jpg");
-            setInterval(movedown, 80);
-            console.log("aaaaaaaaa");
-            break;
-        case 2:
-            fallenRock = new Rocks("./img/2.jpg");
-            setInterval(movedown, 60);
-            break;
-        case 3:
-            fallenRock = new Rocks("./img/3.jpg");
-            setInterval(movedown, 40);
-            break;
-
-    }
-    rocksArray.push(fallenRock);
-}
-
-
-
-
-let startFall = function(){
-    if(counter < 7){
-        determineLevel(1);
-        counter++;
-    }else if(counter >= 7 && counter < 15){
-        determineLevel(2);
-        counter++;
-    }else{
-        determineLevel(3);
-    }
-}
-var interval = setInterval(startFall,1000)
-let deletRocks = function(){
-    let counterArray=0;
-    while (rocksArray[counterArray].rock.style.bottom == "-100px"){
-        rocksArray.shift();
-    }
-};
-
-
-
-// let spaceShip = document.getElementById("spaceShip");
-
-// let addTheSpaceShip = function () {
-//
-//
-//     spaceShip.style.display = "inline-block";
-//     spaceShip.style.left = `px`;
-// };
-// addTheSpaceShip();
 
 class SpaceShip {
     constructor(src,id) {
@@ -120,7 +47,7 @@ class SpaceShip {
         this.spaceShip.id = `${id}`;
         this.spaceShip.style.position = "absolute";
         this.spaceShip.style.top = `${570}px`;
-        this.spaceShip.style.left = `${(windowWidth - this.spaceShip.offsetWidth) / 2}px`;
+        this.spaceShip.style.left = `${(windowWidth - 125) / 2}px`;
         this.spaceShip.style.width="125px";
         this.spaceShip.style.height="140px";
         container.appendChild(this.spaceShip);
@@ -139,9 +66,23 @@ class FireBall {
         container.appendChild(this.fireBall);
     }
 }
-//creating the
-let playerSpaceShip = new SpaceShip("./img/spaceShip.png",1);
-//Stand Alone Functions
+
+//====================Global stand Alone Functions==============
+// check the current level
+let levelUp = function () {
+    if (currentScore > 0) {
+        return 1;
+    } else if (currentScore >= 200 && currentScore < 600) {
+        return 2;
+    } else if (currentScore >= 600 && currentScore < 1200){
+        return 3;
+    }
+    else {
+        return 4;
+    }
+};
+
+//Adding the firing functions
 let fire = function () {
     let fireball = new FireBall("./img/fire.gif");
     fireballs.push(fireball);
@@ -150,26 +91,46 @@ let fire = function () {
     };
     setInterval(moveUp, 40);
 };
-
-
-//Event Functions
-let controlSpaceShip = function (event) {
-    if (keyPressed["ArrowRight"]) {
-        if (playerSpaceShip.spaceShip.offsetLeft < 1400) {
-            playerSpaceShip.spaceShip.style.left = `${playerSpaceShip.spaceShip.offsetLeft += 20}px`;
+//Falling rocks
+let fallenRock = null;
+let chosenRock = function (level) {
+        switch (level) {
+            case 1:
+                fallenRock = new Rocks("./img/rock1.gif");
+                break;
+            case 2:
+                fallenRock = new Rocks("./img/rock2.gif");
+                break;
+            case 3:
+                fallenRock = new Rocks("./img/3.jpg");
+                break;
         }
+        return fallenRock;
+    };
+let fallingRocks = function (level) {
+    let currentFallingRock = chosenRock(level);
+    let fallingInterval = null;
+    switch (level) { // chooses the speed
+        case 1 :
+            fallingInterval = 100;
+            break;
+        case 2 :
+            fallingInterval = 80;
+            break;
+        case 3 :
+            fallingInterval = 60;
+            break;
+        case 4 :
+            fallingInterval = 40;
+            break
     }
-    if (keyPressed["ArrowLeft"]) {
-        if (playerSpaceShip.spaceShip.offsetLeft > 20) {
-            playerSpaceShip.spaceShip.style.left = `${playerSpaceShip.spaceShip.offsetLeft -= 20}px`;
-        }
-    }
-
+    let moveDown = function () {
+        currentFallingRock.rock.style.top = `${currentFallingRock.rock.offsetTop + 10}px`
+    };
+    setInterval(moveDown, fallingInterval);
 };
-//===================================================================
-// The Game movement
-//-------------------
-// 1 - keyboard listeners.
+
+//-------------keyboard listeners--------------
 let keyPressed = [];
 let keyPress = function (event) {
     keyPressed[event.code] = true;
@@ -184,33 +145,87 @@ document.addEventListener("keydown", keyPress);
 document.addEventListener("keyup", keyReleased);
 
 
-//====== functions that needed to be fired regularly========
-//Fire checker
-setInterval(() => {
-    if (keyPressed["Space"]) fire()
-}, 150);
-//checker for every thing else
-setInterval(() => {
-    controlSpaceShip();
-    for (let i = 0; i < fireballs.length; i++) {
-        currentBall = document.getElementById(fireballs[i].fireBall.id);
-        if (currentBall.offsetTop < 50) {
-            currentBall.remove();
-            fireballs.splice(i,1);
-        }
-        for (let j = 0;j<sq.length;j++) {
-            currentRock = document.getElementById(sq[j].id);
-            if ( Math.abs((sq[j].offsetTop + sq[j].offsetHeight) - currentBall.offsetTop) <20
-                && currentBall.offsetLeft >= (currentRock.offsetLeft-currentBall.offsetWidth+5)
-                && currentBall.offsetLeft <= (currentRock.offsetLeft+currentRock.offsetWidth))
-            {
-                currentBall.remove();
-                fireballs.splice(i,1);
-                currentRock.remove();
-                sq.splice(j,1);
+//======================Playing the game ======================
+let play = function()
+{
+    //----------Session variables -------------
+    playerSpaceShip = new SpaceShip("./img/spaceShip.png", -1);
+    currentLevel = 1;
+    currentScore = 0;
+    currentSurvivalTime = 0;
+    //Start falling rocks
+    setTimeout(()=>{
+                    setInterval(()=>{fallingRocks(1)},500)
+    },3000);
+
+//Event Functions
+    let controlSpaceShip = function (event) {
+        if (keyPressed["ArrowRight"]) {
+            if (playerSpaceShip.spaceShip.offsetLeft < 1400) {
+                playerSpaceShip.spaceShip.style.left = `${playerSpaceShip.spaceShip.offsetLeft += 20}px`;
             }
         }
-    }
+        if (keyPressed["ArrowLeft"]) {
+            if (playerSpaceShip.spaceShip.offsetLeft > 20) {
+                playerSpaceShip.spaceShip.style.left = `${playerSpaceShip.spaceShip.offsetLeft -= 20}px`;
+            }
+        }
+        if (keyPressed["ArrowDown"]) {
+            if (playerSpaceShip.spaceShip.offsetTop < 570) {
+                playerSpaceShip.spaceShip.style.top = `${playerSpaceShip.spaceShip.offsetTop += 20}px`;
+            }
+        }
+        if (keyPressed["ArrowUp"]) {
+            if (playerSpaceShip.spaceShip.offsetTop > 10) {
+                playerSpaceShip.spaceShip.style.top = `${playerSpaceShip.spaceShip.offsetTop -= 20}px`;
+            }
+        }
+    };
+//===================================================================
+// The Game movement
+//-------------------
 
-}, 100);
 
+
+//====== functions that needed to be fired regularly========
+//Fire checker
+    setInterval(() => {
+        if (keyPressed["ControlLeft"]) fire()
+    }, 150);
+//checker for every thing else
+    setInterval(() => {
+        controlSpaceShip();
+        for (let i = 0; i < fireballs.length; i++) {
+            currentBall = document.getElementById(fireballs[i].fireBall.id);
+            if (currentBall.offsetTop < 50) {
+                currentBall.remove();
+                fireballs.splice(i, 1);
+            }
+            for (let j = 0; j < rocksArray.length; j++) {
+                currentRock = document.getElementById(rocksArray[j].id);
+                //Check fire collision
+                if (Math.abs((currentRock.offsetTop + currentRock.offsetHeight) - currentBall.offsetTop) < 20
+                    && currentBall.offsetLeft >= (currentRock.offsetLeft - currentBall.offsetWidth + 5)
+                    && currentBall.offsetLeft <= (currentRock.offsetLeft + currentRock.offsetWidth)) {
+                    currentBall.remove();
+                    fireballs.splice(i, 1);
+                    currentRock.remove();
+                    rocksArray.splice(j, 1);
+                }
+                //Check when rock reach the end of the screen
+            }
+        }
+        for (let k = 0; k < rocksArray.length; k++) {
+            let rockAtBottom = document.getElementById(rocksArray[k].id);
+            if (rockAtBottom.offsetTop + rockAtBottom.offsetHeight > window.innerHeight - 70) {
+                rockAtBottom.remove();
+                rocksArray.splice(k, 1);
+            }
+        }
+
+        //Check the current level
+        currentLevel = levelUp();
+
+    }, 100);
+};
+play();
